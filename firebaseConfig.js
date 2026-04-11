7df14cb81b95e13197c56cc9fc21310e23f8caad
ishakalistair-cell <ishakalistair@gmail.com>
2026-04-10 23:17:09 -0400

    Added firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA794KVWfzLtSfxJgGUuJo6Mkqolb5tLKo",
+
+  authDomain: "avatarhubio.firebaseapp.com",
+
+  projectId: "avatarhubio",
+
+  storageBucket: "avatarhubio.firebasestorage.app",
+
+  messagingSenderId: "1055546015883",
+
+  appId: "1:1055546015883:web:e3b67873277f82cbf8148e"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
